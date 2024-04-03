701b425316480eba4b8f6d713513f533222b453c
Ante Bucan <ab8390@g.rit.edu>
2024-04-03 14:57:49 +0200

    ..

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,7 +2,7 @@ const SibApiV3Sdk = require('sib-api-v3-sdk');
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 const apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = '';
+apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
 
 var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
